361664a47a4ac33eaadf01cfa1511872ef5f6ac5
guptak-hash <guptakishan1123@gmail.com>
2025-06-08 20:05:49 +0530

    craeted products details page

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,20 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from './components/Shared/Navbar'
+// App.js
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+import ProductDetailsPage from './pages/ProductDetailsPage';
+import CartPage from './pages/CartPage';
+import LoginPage from './pages/Auth/LoginPage';
+import RegisterPage from './pages/Auth/RegisterPage';
+import ProtectedRoute from './components/Shared/ProtectedRoute';
+import Navbar from './components/Shared/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  
-     <BrowserRouter>
-<Navbar/>
-     </BrowserRouter>
- 
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        
+        {/* Protected routes */}
+        <Route
+          path="/products/:id"
+          element={
+            <ProtectedRoute>
+              <ProductDetailsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <ProtectedRoute>
+              <CartPage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
