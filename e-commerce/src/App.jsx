831e30fd58ec6d8797110e4f9316bf17bcbdeb9c
guptak-hash <guptakishan1123@gmail.com>
2025-06-08 18:57:51 +0530

    Configure Redux store with auth, products, and cart slices

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from './components/Shared/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+     <BrowserRouter>
+<Navbar/>
+     </BrowserRouter>
+ 
+  )
+}
+
+export default App
